48445dfc88dd9a266ba178e3b085183454facdba
Best Telegram Marketing Bots 2025 <139961701+QQSHILL@users.noreply.github.com>
2025-03-20 16:02:20 +0800

    Create telegram-invite-bot.py

---
+++ telegram-invite-bot.py
@@ -0,0 +1,15 @@
+"""
+Telegram Invite Bot - Information Script
+Created on: March 20, 2025
+Author: QQSHILL
+
+This script informs users about the Telegram Invite Bot and where to learn more.
+"""
+
+def show_info():
+    print("🚀 Welcome to Telegram Invite Bot 🚀")
+    print("This bot helps you extract and invite members to your Telegram groups and channels.")
+    print("🔗 Learn more and get started here: https://www.qqshill.com/telegram-invite-bot")
+
+if __name__ == "__main__":
+    show_info()
